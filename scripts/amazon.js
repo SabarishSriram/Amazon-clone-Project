6ec42007ed3d9dc3e2464a85a430d9aa0c35d8ef
sabarish_2005 <vbsabarishsriram@gmail>
2024-01-04 20:24:29 +0530

    Adding products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,18 +2,114 @@ const products=[{
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
     name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating:{stars:4.5,count:87},
-    pricecents: 1090,
+    priceCents: 1090,
 },{
     image:"images/products/intermediate-composite-basketball.jpg",
     name:"Intermediate Size Basketball",
     rating:{stars:4, count:127},
-    pricecents:2095
+    priceCents:2095
 },{
     image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     name:"Adults Plain Cotton T-Shirt - 2 Pack",
     rating:{stars:4.5,count:56},
-    pricecents:7990
-}]
+    priceCents:7990
+},{
+    image:"images/products/black-2-slot-toaster.jpg",
+    name:"2 Slot Toaster - Black",
+    rating:{stars:5,count:2197},
+    priceCents:1899
+},{
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: "images/products/knit-athletic-sneakers-gray.jpg",
+    name: "Waterproof Knit Athletic Sneakers - Gray",
+    rating: {
+      stars: 4,
+      count: 89
+    },
+    priceCents: 3390,
+    keywords: [
+      "shoes",
+      "running shoes",
+      "footwear"
+    ]
+},{
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: "images/products/round-sunglasses-black.jpg",
+    name: "Round Sunglasses",
+    rating: {
+      stars: 4.5,
+      count: 30
+    },
+    priceCents: 1560,
+    keywords: [
+      "accessories",
+      "shades"
+    ]
+},{
+    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    image: "images/products/blackout-curtain-set-beige.webp",
+    name: "Blackout Curtains Set 4-Pack - Beige",
+    rating: {
+      stars: 4.5,
+      count: 232
+    },
+    priceCents: 4599,
+    keywords: [
+      "bedroom",
+      "curtains",
+      "home"
+    ]
+},{
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: "images/products/facial-tissue-2-ply-18-boxes.jpg",
+    name: "Ultra Soft Tissue 2-Ply - 18 Box",
+    rating: {
+      stars: 4,
+      count: 99
+    },
+    priceCents: 2374,
+    keywords: [
+      "kleenex",
+      "tissues",
+      "kitchen",
+      "tissues box",
+      "napkins"
+    ]
+},{
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: "images/products/sky-flower-stud-earrings.webp",
+    name: "Sterling Silver Sky Flower Stud Earrings",
+    rating: {
+      stars: 4.5,
+      count: 52
+    },
+    priceCents: 1799,
+    keywords: [
+      "jewelry",
+      "accessories",
+      "womens"
+    ]
+},{
+    id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    image: "images/products/women-stretch-popover-hoodie-black.jpg",
+    name: "Women's Stretch Popover Hoodie",
+    rating: {
+      stars: 4.5,
+      count: 2465
+    },
+    priceCents: 1374,
+    keywords: [
+      "hooded",
+      "hoodies",
+      "sweaters",
+      "womens",
+      "apparel"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+},
+
+]
 
 
 let html='';
@@ -38,7 +134,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-            $${(product.pricecents/100).toFixed(2)}
+            $${(product.priceCents/100).toFixed(2)}
         </div>
 
         <div class="product-quantity-container">
